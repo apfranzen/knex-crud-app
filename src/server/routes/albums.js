8fb82140fa63ccc115ab6bfe7bd72174099c280d
Adam Franzen <adamfranzen@gmail.com>
2016-09-06 15:09:47 -0600

    pulled in knex

--- src/server/routes/albums.js
+++ src/server/routes/albums.js
@@ -1,11 +1,21 @@
 const router = require ('express').Router();
 
+
+// GET album landing page
 router.get('/', (req, res, next) => {
   res.render('albums/index', {title: 'Albums'});
 });
 
+// GET new album form page
 router.get('/new', (req, res, next) => {
   res.render('albums/new', {title: 'New Album'});
+});
+
+// POST a new album
+router.post('/', (req, res, next) => {
+  // logged out req.body to confirm the form submission can be captured
+  console.log(req.body);
+  res.end();
 });
 
 module.exports = router;
