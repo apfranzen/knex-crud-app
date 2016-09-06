ceff66debf2ba880418e0c572e791d5095528b85
Adam Franzen <adamfranzen@gmail.com>
2016-09-06 16:39:22 -0600

    submitted albums now showing

--- src/server/routes/albums.js
+++ src/server/routes/albums.js
@@ -1,25 +1,38 @@
 const router = require ('express').Router();
 const knex = require('../db/knex');
+const genres = require('../genres.json');
 
 function Albums() { return knex('albums'); }
 
 
 // GET album landing page
 router.get('/', (req, res, next) => {
-  res.render('albums/index', {title: 'Albums'});
+  Albums().select().then(records => {
+    res.render('albums/index', {
+      title: 'Albums',
+      albums: records
+    });
+  });
 });
 
 // GET new album form page
 router.get('/new', (req, res, next) => {
-  res.render('albums/new', {title: 'New Album'});
+  res.render('albums/new', {title: 'New Album', genres: genres});
 });
 
 // POST a new album
 router.post('/', (req, res, next) => {
+  let newAlbum = {
+    artist: req.body.artist,
+    name: req.body.name,
+    genre: req.body.genre,
+    stars: parseInt(req.body.stars),
+    explicit: Boolean(req.body.explicit)
+  };
   // logged out req.body to confirm the form submission can be captured
   // console.log(req.body);
   // res.end();
-  Albums().insert({ name: req.body.name })
+  Albums().insert( newAlbum )
   .then(() => {
     res.redirect('/albums');
   });
